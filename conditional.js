57a47cd07843ddfd0b6d18c7a8156898a4c9a5d8
Alvaro Frias <alvarofriasruiz03@gmail.com>
2023-12-14 12:54:32 +0100

    Incluir el else if

--- conditional.js
+++ conditional.js
@@ -10,21 +10,45 @@ const noWater = false;
 const canShower = !noWater;
 
 if (canShower) {
-    console.log("Estare limpio");
+	console.log("Estare limpio");
 } else {
-    console.log("olere mal");
+	console.log("olere mal");
 }
 
-const users = {name: "Alvaro"};
+const users = { name: "Alvaro" };
 
 if (!users) {
-    console.log("No hay usuarios");
+	console.log("No hay usuarios");
 } else {
-    console.log(users);
+	console.log(users);
 }
 
 if (Boolean(users) === false) {
-    console.log("No hay usuarios");
+	console.log("No hay usuarios");
 } else {
-    console.log(users);
+	console.log(users);
+}
+
+// Ejemplo else if
+
+const grade = 85;
+
+if (grades >= 90) {
+	console.log("Aprobaste con sobresaliente");
+} else if (grades >= 70) {
+	console.log("Aprobaste con notable");
+} else if (grades >= 50) {
+	console.log("Aprobaste con suficiente");
+} else {
+	console.log("Suspendiste amigo");
+}
+
+const age = 25;
+
+if (age >= 65) {
+	console.log("Estas jubilado");
+} else if (age >= 18 && age < 65) {
+	console.log("Cotiza");
+} else {
+	console.log("A estudiar");
 }
