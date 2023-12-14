0f766a1e7eda9d86a119f4156cde2f91d480f464
Alvaro Frias <alvarofriasruiz03@gmail.com>
2023-12-14 12:54:54 +0100

    Crear el archivo y hacer bucles en el

---
+++ loops.js
@@ -0,0 +1,90 @@
+// Bucles
+
+// While
+
+let count = 10;
+const max = 10;
+
+while (count <= max) {
+	// console.log(`El valor del count es ${count}`);
+	count++;
+}
+
+// Do While
+
+do {
+	// console.log(`El valor del count es ${count}`);
+	count++;
+} while (count <= max);
+
+// For
+
+for (let i = 0; i < max; i++) {
+	// console.log(`El valor del i es ${i}`);
+}
+
+const array = ["nacho", "ALvaro", "PAblo", "nerea", "marco"];
+
+for (let i = 0; i < array.length; i++) {
+	// console.log(`Hola ${i}`);
+}
+
+const numberArray = [1, 2, 3, 4, 5, 6, 7, 8];
+
+for (let i = 0; i < numberArray.length; i++) {
+	numberArray[i] = numberArray[i] * 2;
+	// console.log(numberArray[i]);
+}
+
+// Mostrar por consola a traves de los bucles while, do while y for numeros del 0 al 25
+
+let cuenta = 0;
+
+while (count <= 25) {
+	// console.log(`El valor del count es ${count}`);
+	count++;
+}
+
+do {
+	// console.log(`El valor del count es ${count}`);
+	count++;
+} while (count <= 25);
+
+// For
+
+for (let i = 0; i < 25; i++) {
+	// console.log(`El valor del i es ${i}`);
+}
+
+// Hacer una cuenta atras desde 10 hasta 0 y al final felicitar el año nuevo
+
+for (let i = 10; i >= 0; i--) {
+	if (i === 0) {
+		// console.log(i);
+		// console.log("Feliz Año Nuevo!!");
+	} else {
+		// console.log(i);
+	}
+}
+
+let i = 10;
+// while (i >= 0) {
+// 	if (i === 0) {
+// 		console.log(i);
+// 		console.log("Feliz Año Nuevo!!");
+// 	} else {
+// 		console.log(i);
+// 	}
+
+// 	i--;
+// }
+
+// do {
+// 	if (i === 0) {
+// 		console.log(i);
+// 		console.log("Feliz Año Nuevo!!");
+// 	} else {
+// 		console.log(i);
+// 	}
+// 	i--;
+// } while (i >= 0);
