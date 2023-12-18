1caa9bd51fde7089f95935dc945ef69132ac1eaf
Alvaro Frias <alvarofriasruiz03@gmail.com>
2023-12-18 12:38:50 +0100

    hacerlo todo funciones

--- conditional.js
+++ conditional.js
@@ -7,84 +7,97 @@
 
 const noWater = false;
 
-const canShower = !noWater;
+function ducha() {
+	const canShower = !noWater;
 
-if (canShower) {
-	console.log("Estare limpio");
-} else {
-	console.log("olere mal");
+	if (canShower) {
+		console.log("Estare limpio");
+	} else {
+		console.log("olere mal");
+	}
 }
 
-const users = { name: "Alvaro" };
+function usuarios() {
+	const users = { name: "Alvaro" };
 
-if (!users) {
-	console.log("No hay usuarios");
-} else {
-	console.log(users);
-}
+	if (!users) {
+		console.log("No hay usuarios");
+	} else {
+		console.log(users);
+	}
 
-if (Boolean(users) === false) {
-	console.log("No hay usuarios");
-} else {
-	console.log(users);
+	if (Boolean(users) === false) {
+		console.log("No hay usuarios");
+	} else {
+		console.log(users);
+	}
 }
 
 // Ejemplo else if
 
-const grades = 85;
-
-if (grades >= 90) {
-	console.log("Aprobaste con sobresaliente");
-} else if (grades >= 70) {
-	console.log("Aprobaste con notable");
-} else if (grades >= 50) {
-	console.log("Aprobaste con suficiente");
-} else {
-	console.log("Suspendiste amigo");
+function gradesIf(grades) {
+	if (grades >= 90) {
+		console.log("Aprobaste con sobresaliente");
+	} else if (grades >= 70) {
+		console.log("Aprobaste con notable");
+	} else if (grades >= 50) {
+		console.log("Aprobaste con suficiente");
+	} else {
+		console.log("Suspendiste amigo");
+	}
 }
 
-const age = 25;
-
-if (age >= 65) {
-	console.log("Estas jubilado");
-} else if (age >= 18 && age < 65) {
-	console.log("Cotiza");
-} else {
-	console.log("A estudiar");
+function ageIf(AGE) {
+	if (age >= 65) {
+		console.log("Estas jubilado");
+	} else if (age >= 18 && age < 65) {
+		console.log("Cotiza");
+	} else {
+		console.log("A estudiar");
+	}
 }
 
 // Estructura switch
 
+function switchFruta(fruit) {
+	switch (fruit) {
+		case "melon":
+			console.log("Que rico el melon en diciembre");
+			break;
+
+		case "papaya":
+			console.log("Que fruta tan tropical");
+			break;
+
+		case "sandia":
+			console.log("Has elegido la sandia");
+			break;
+
+		default:
+			break;
+	}
+}
+
+function switchGrades(grades) {
+	switch (true) {
+		case grades >= 90:
+			console.log("Aprobaste con sobresaliente");
+			break;
+		case grades >= 70:
+			console.log("Aprobaste con notable");
+			break;
+		case grades >= 50:
+			console.log("Aprobaste con suficiente");
+			break;
+
+		default:
+			console.log("Suspendiste amigo");
+			break;
+	}
+}
+
+const age = 25;
+
+const grades = 85;
+
 let fruit = "melon";
-switch (fruit) {
-	case "melon":
-		console.log("Que rico el melon en diciembre");
-		break;
-
-	case "papaya":
-		console.log("Que fruta tan tropical");
-		break;
-
-	case "sandia":
-		console.log("Has elegido la sandia");
-		break;
-
-	default:
-		break;
-}
-
-switch (true) {
-	case grades >= 90:
-		console.log("Aprobaste con sobresaliente");
-		break;
-	case grades >= 70:
-		console.log("Aprobaste con notable");
-		break;
-	case grades >= 50:
-		console.log("Aprobaste con suficiente");
-		break;
-
-	default:
-		console.log("Suspendiste amigo");
-		break;
-}
