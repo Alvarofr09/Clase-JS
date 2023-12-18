96d04afeef3570210273941e2ceba3e470f943f7
Alvaro Frias <alvarofriasruiz03@gmail.com>
2023-12-18 12:10:12 +0100

    variables como funciones y funciones flechas

--- function.js
+++ function.js
@@ -5,7 +5,7 @@
 console.log("<---------------Funcion nombrada con retorno------------->");
 console.log("<---------------Funcion covertir string a numero------------->");
 function coverStringToNumber(string) {
-    return Number(string);
+	return Number(string);
 }
 
 console.log(coverStringToNumber("3"));
@@ -16,19 +16,39 @@ console.log("<---------------Funcion que nos cambia el estado------------->");
 let state = 1;
 
 function changeUserStatus() {
-    state = 2;
+	state = 2;
 }
 
 console.log("Estado del usuario antes " + state);
+
 changeUserStatus();
+
 console.log("Estado del usuario despues " + state);
 
-console.log("<---------------Funcion que nos da los milisegundos y luego los cambia------------->");
+console.log(
+	"<---------------Funcion que nos da los milisegundos y luego los cambia------------->"
+);
 let currentTime = new Date().getTime(); // Nos da los milisegundos desde 1/1/1970
 console.log(currentTime);
 function getCurrentTime() {
-    currentTime = new Date().getTime();
-    console.log(currentTime);
+	currentTime = new Date().getTime();
+	console.log(currentTime);
 }
 
 setTimeout(getCurrentTime, 1000);
+
+// Variables con funciones como valor
+
+const coverStringToNumber2 = function (string) {
+	return Number(string);
+};
+
+console.log(coverStringToNumber2("4"));
+
+// Funcion flecha
+
+const coverStringToNumber3 = (string) => Number(string);
+
+const number = coverStringToNumber3("45");
+
+console.log(number);
