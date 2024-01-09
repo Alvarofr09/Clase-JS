21955be4a2f78f322781608e6381278a6f9f6d87
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-01-09 13:31:14 +0100

    ejercicio permutacion 2.0

--- permutacion.js
+++ permutacion.js
@@ -1,8 +1,30 @@
+function generarNumeroAleatorio() {
+	return Math.floor(Math.random() * 3) + 1;
+}
+
+function generarNumeroDeTresCifras() {
+	const cifra1 = generarNumeroAleatorio();
+	const cifra2 = generarNumeroAleatorio();
+	const cifra3 = generarNumeroAleatorio();
+
+	const numeroDeTresCifras = cifra1 * 100 + cifra2 * 10 + cifra3;
+
+	return numeroDeTresCifras;
+}
+
 function permutar() {
+	const numeroAleatorio = generarNumeroDeTresCifras();
+	let contador = 0;
+	console.log(numeroAleatorio);
 	for (let i = 1; i < 4; i++) {
 		for (let j = 1; j < 4; j++) {
 			for (let k = 1; k < 4; k++) {
-				console.log(`${i} ${j} ${k}`);
+				let numero = `${i}${j}${k}`;
+				contador++;
+
+				if (numeroAleatorio == numero) {
+					console.log(`Has acertado el numero al intento numero: ${contador}`);
+				}
 			}
 		}
 	}
