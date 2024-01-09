9c92426c987182bed0cab5c78eb23fdfb79fe1da
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-01-09 12:57:26 +0100

    ejercicio de permutar 1.0

---
+++ permutacion.js
@@ -0,0 +1,11 @@
+function permutar() {
+	for (let i = 1; i < 4; i++) {
+		for (let j = 1; j < 4; j++) {
+			for (let k = 1; k < 4; k++) {
+				console.log(`${i} ${j} ${k}`);
+			}
+		}
+	}
+}
+
+permutar();
