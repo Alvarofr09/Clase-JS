79da985b73b682d3b046546c8a30775b8cc1a588
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-01-24 13:56:29 +0100

    Clase de asincronia, con promesas

--- Asincronia/asincronia.js
+++ Asincronia/asincronia.js
@@ -14,47 +14,93 @@
 // console.timeEnd("Loop took");
 // console.log("termino el bucle ", total);
 
-function makeRequest(method, url, callback) {
-	const xhr = new XMLHttpRequest();
-	xhr.open(method, url);
-	xhr.responseType = "json";
-	xhr.onload = () => {
-		if (xhr.status >= 200 && xhr.status < 300) {
-			callback(null, xhr.response);
-		} else {
-			callback(new Error(xhr.statusText), null);
-		}
-	};
+// function makeRequest(method, url, callback) {
+// 	const xhr = new XMLHttpRequest();
+// 	xhr.open(method, url);
+// 	xhr.responseType = "json";
+// 	xhr.onload = () => {
+// 		if (xhr.status >= 200 && xhr.status < 300) {
+// 			callback(null, xhr.response);
+// 		} else {
+// 			callback(new Error(xhr.statusText), null);
+// 		}
+// 	};
 
-	xhr.onerror = () => {
-		callback(new Error("Network error"), null);
-	};
+// 	xhr.onerror = () => {
+// 		callback(new Error("Network error"), null);
+// 	};
 
-	xhr.send();
+// 	xhr.send();
+// }
+
+// console.time("Fetch took");
+// makeRequest("GET", `${baseUrl}/users/1`, (err, users) => {
+// 	console.log({ users });
+// 	console.timeEnd("Fetch took");
+// 	makeRequest("GET", `${baseUrl}/posts?userId=${users.id}`, (err, posts) => {
+// 		const post = posts[0];
+// 		makeRequest(
+// 			"GET",
+// 			`${baseUrl}/comments?postId=${post.id}`,
+// 			(err, comments) => {
+// 				console.log({ comments });
+// 				console.timeEnd("Fetch took");
+// 			}
+// 		);
+// 	});
+// });
+
+// // Inversion de control
+// paypal.createOrder(orderInfo, (err, createdOrder) => {
+// 	console.log("Esto lo ejecutaria el SDK Software Developer Kit");
+// });
+
+// // Promesas soluciona la inversion de control
+// paypal
+// 	.createOrder(orderInfo)
+// 	.then(() => {
+// 		console.log("Esto se ejecutaria en nuestro codigo");
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 	});
+
+function makeRequest(method, url) {
+	const promise = new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
+		xhr.open(method, url);
+		xhr.responseType = "json";
+		xhr.onload = () => {
+			if (xhr.status >= 200 && xhr.status < 300) {
+				resolve(xhr.response);
+			} else {
+				reject(new Error(xhr.statusText));
+			}
+		};
+
+		xhr.onerror = () => {
+			reject(new Error("Network error"));
+		};
+
+		xhr.send();
+	});
+	return promise;
 }
 
+// fullfilled, pending y rejected Estos son los 3 estados de las promesas
 const baseUrl = "https://jsonplaceholder.typicode.com";
 console.time("Fetch took");
-makeRequest("GET", `${baseUrl}/users/1`, (err, users) => {
-	// console.log({ users });
-	// console.timeEnd("Fetch took");
-	makeRequest("GET", `${baseUrl}/posts?userId=${users.id}`, (err, posts) => {
-		const post = posts[0];
-		makeRequest(
-			"GET",
-			`${baseUrl}/comments?postId=${post.id}`,
-			(err, comments) => {
-				console.log({ comments });
-				console.timeEnd("Fetch took");
-			}
-		);
+const promise = makeRequest("get", `${baseUrl}/users/1`);
+console.log({ promise });
+
+promise
+	.then((res) => {
+		console.log("Todo salio bien ", res);
+		console.timeEnd("Fetch took");
+	})
+	.catch((err) => {
+		console.log("Todo salio mal ", err);
+	})
+	.finally(() => {
+		console.log("Esto se ejecuta SIEMPRE");
 	});
-});
-
-// Inversion de control
-paypal.createOrder(orderInfo, (err, createdOrder) => {
-	console.log("Esto lo ejecutaria el SDK Software Developer Kit");
-});
-
-// Promsesas soluciona la inversion de control
-paypal.createOrder(orderInfo);
