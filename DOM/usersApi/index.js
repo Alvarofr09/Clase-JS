4fafdabcf2c14446fe59aab3dd89a07bdbd9e8e2
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-01-25 14:01:17 +0100

    ejemplo reutilizacion

--- DOM/usersApi/index.js
+++ DOM/usersApi/index.js
@@ -15,6 +15,13 @@ async function getUsers() {
 	return users;
 }
 
+async function getData(url) {
+	const response = await fetch(url);
+	const data = await response.json();
+
+	return data;
+}
+
 async function createUsersList() {
 	const users = await getUsers();
 
