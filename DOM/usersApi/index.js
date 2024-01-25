b883be711b474f63805417929c94f59f249f9086
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-01-25 13:59:54 +0100

    ejercicio con la api (sacar usuarios)

---
+++ DOM/usersApi/index.js
@@ -0,0 +1,29 @@
+const contactList = document.querySelector("#contactList");
+const baseUrl = "https://jsonplaceholder.typicode.com";
+
+function myFetch(url) {
+	return fetch(url).then((res) => {
+		return res.json();
+	});
+}
+
+async function getUsers() {
+	// const users = await myFetch(`${baseUrl}/users`);
+	const response = await fetch(`${baseUrl}/users`);
+	const users = await response.json();
+
+	return users;
+}
+
+async function createUsersList() {
+	const users = await getUsers();
+
+	for (let i = 0; i < users.length; i++) {
+		const li = document.createElement("li");
+		li.setAttribute("id", users[i].id);
+		li.textContent = users[i].name;
+
+		contactList.appendChild(li);
+	}
+}
+createUsersList();
