07e90cb81e78ae0e11103e7d9ae1de67360824e1
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-01-31 09:42:23 +0100

    hacer la llamada a la api

--- DOM/CRUD/index.js
+++ DOM/CRUD/index.js
@@ -1,3 +1,10 @@
 const createForm = document.querySelector("#createUserForm");
 const userContainer = document.querySelector("#userContainer");
 const updateForm = document.querySelector("#updateUserForm");
+
+async function getUsers() {
+	try {
+		const response = await fetch("https://reqres.in/api/users");
+		const users = await response.json();
+	} catch (error) {}
+}
