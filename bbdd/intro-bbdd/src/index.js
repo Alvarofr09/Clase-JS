284998c28678d6ecfd8d4c9a1ff4d434eb342196
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-02-19 10:28:21 +0100

    iniciar la conexion a la base de datos y hacer un select

--- bbdd/intro-bbdd/src/index.js
+++ bbdd/intro-bbdd/src/index.js
@@ -1,1 +1,12 @@
 import { pool } from "./conectionMysql.js";
+
+function getPersonajes() {
+	try {
+		const result = pool.query("SELECT * FROM users");
+		console.log(result);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+getPersonajes();
