d9245c3d377e0d0119658d4ac3c9b13c625391ae
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-02-26 11:09:56 +0100

    hacer los endpoints de todos los estudiantes y de uno

--- bbdd/crud-node-js/server/src/index.js
+++ bbdd/crud-node-js/server/src/index.js
@@ -1,1 +1,43 @@
-console.log("Adios");
+import express from "express";
+import {
+	getEstudiantes,
+	getEstudianteByid,
+} from "./api/estudiantes/estudiantes.js";
+
+// Crear la constante que llama a express
+const app = express();
+
+// Funcion que usa express
+app.use(express.json());
+
+// Poner el puerto que vaya a usar
+const PORT = 8000;
+
+// Endpoint de todos los estudiantes
+app.get("/students", async (req, res) => {
+	try {
+		const result = await getEstudiantes();
+		res.send(result);
+	} catch (error) {
+		errorApi(res, error);
+	}
+});
+
+// Endpoint de un estudiante
+app.get("/students/:id", async (req, res) => {
+	try {
+		const result = await getEstudianteByid(req.params.id);
+		res.send(result);
+	} catch (error) {
+		errorApi(res, error);
+	}
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => console.log(`Servidor escuchando en el puerto ${PORT}`));
+
+// Funcion para usar en el catch cada vez que se hace un endpoint
+function errorApi(res, error) {
+	console.error("Error al realizar la consulta", error);
+	res.status(500).json({ error: "Error interno del servidor" });
+}
