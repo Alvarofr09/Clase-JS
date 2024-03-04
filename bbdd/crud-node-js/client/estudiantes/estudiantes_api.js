6458d4f127b036667c4e18cec6501b854983a985
Alvarofr09 <alvarofriasruiz03@gmail.com>
2024-03-04 18:16:48 +0100

    empezar la funcion de borrar

--- bbdd/crud-node-js/client/estudiantes/estudiantes_api.js
+++ bbdd/crud-node-js/client/estudiantes/estudiantes_api.js
@@ -4,12 +4,24 @@ const editStudentURL = URL + "/update-students/";
 const deleteStudentURL = URL + "/delete-students/";
 
 async function updateStudent(id) {
-	console.log("Vas a editar al estudiante con el id: " + id);
+	try {
+		console.log("Vas a editar al estudiante con el id: " + id);
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 async function deleteStudent(id) {
+	const url = deleteStudent + id;
 	try {
-		console.log("Vas a borrar al estudiante con el id: " + id);
+		const response = await fetch(url, {
+			method: "DELETE",
+		});
+		if (response.status === 204) {
+			alert("Estudiante eliminado");
+			const userToDelete = document.querySelector(`[data-id = "${id}"]`);
+			userToDelete.remove();
+		}
 	} catch (error) {
 		console.log(error);
 	}
