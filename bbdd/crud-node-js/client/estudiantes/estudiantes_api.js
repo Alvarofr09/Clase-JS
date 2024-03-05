262a2ec6c644d7b66fcc4a7b9c227d7c8893d98d
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-05 14:00:57 +0100

    hacer la funcion de crear estudiante

--- bbdd/crud-node-js/client/estudiantes/estudiantes_api.js
+++ bbdd/crud-node-js/client/estudiantes/estudiantes_api.js
@@ -1,18 +1,58 @@
 import { URL } from "./estudiantes.js";
 
-const editStudentURL = URL + "/update-students/";
-const deleteStudentURL = URL + "/delete-students/";
+const addStudentURL = URL + "add-students/";
+const editStudentURL = URL + "update-students/";
+const deleteStudentURL = URL + "delete-students/";
+
+async function showDataStudent(id) {
+	try {
+		console.log(URL + `${id}`);
+		const response = await fetch(URL + `${id}`);
+		const data = await response.json();
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
+}
+async function createStudent(Estudiante) {
+	try {
+		const response = await fetch(addStudentURL, {
+			method: "POST",
+			body: JSON.stringify(Estudiante),
+			headers: {
+				"Content-type": "application/json",
+			},
+		});
+
+		if (response.ok) {
+			location.reload();
+		}
+	} catch (error) {
+		console.log(error);
+	}
+}
 
 async function updateStudent(id) {
 	try {
-		console.log("Vas a editar al estudiante con el id: " + id);
+		showDataStudent(id);
+		// const response = await fetch(`${URL}${id}`, {
+		// 	method: "PUT",
+		// 	body: JSON.stringify(Estudiante),
+		// 	headers: {
+		// 		"Content-type": "application/json",
+		// 	},
+		// });
+
+		// if (response.ok) {
+		// 	location.reload();
+		// }
 	} catch (error) {
 		console.log(error);
 	}
 }
 
 async function deleteStudent(id) {
-	const url = deleteStudent + id;
+	const url = deleteStudentURL + id;
 	try {
 		const response = await fetch(url, {
 			method: "DELETE",
@@ -22,9 +62,46 @@ async function deleteStudent(id) {
 			const userToDelete = document.querySelector(`[data-id = "${id}"]`);
 			userToDelete.remove();
 		}
+		location.reload();
 	} catch (error) {
 		console.log(error);
 	}
 }
 
-export { updateStudent, deleteStudent };
+export { createStudent, updateStudent, deleteStudent };
+
+updateForm.addEventListener("submit", async function (e) {
+	e.preventDefault();
+	const formData = Object.fromEntries(new FormData(this));
+
+	const editedUser = {
+		name: formData.first_name,
+		job: formData.job,
+	};
+	const userId = document.querySelector("[data-id]");
+
+	try {
+		const response = await fetch(
+			`https://reqres.in/api/users/${userId.dataset.id}`,
+			{
+				method: "PUT",
+				body: JSON.stringify(editedUser),
+				headers: {
+					"Content-type": "application/json",
+				},
+			}
+		);
+
+		if (response.ok) {
+			const updateUser = await response.json();
+			const userToUpdate = document.querySelector(
+				`[data-userid = "${userId.dataset.id}"]`
+			);
+			userToUpdate.textContent = updateUser.name;
+			this.reset();
+			updateForm.classList.toggle("hide");
+		}
+	} catch (error) {
+		console.log(error);
+	}
+});
