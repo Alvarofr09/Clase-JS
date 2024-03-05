6b8f80b06f13cba4e6ab0bb84110c71439597106
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2024-03-05 17:31:05 +0100

    poner los campo igual que en clase

--- bbdd/crud-node-js/server/src/database/BBDD.sql
+++ bbdd/crud-node-js/server/src/database/BBDD.sql
@@ -4,15 +4,16 @@ CREATE DATABASE releevant;
 USE releevant;
 
 CREATE TABLE students (
-    ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-    NAME VARCHAR(255) NOT NULL,
-    LAST_NAME VARCHAR(255) NOT NULL,
-    AGE INTEGER NOT NULL,
-    GENDER VARCHAR(255) NOT NULL
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    age INTEGER NOT NULL,
+    gender VARCHAR(255) NOT NULL
 );
 
 INSERT INTO students (NAME, LAST_NAME, AGE, GENDER) VALUES 
 ('Alvaro', 'Frias Ruiz', 20, 'Masculino'),
+('Mireilla', 'Ramirez Camacho', 22, 'Femenino')
 ('Juan', 'Gonzalez', 20, 'Masculino'),
 ('Manoli', 'Pedrosa', 40, 'Femenino')
        
