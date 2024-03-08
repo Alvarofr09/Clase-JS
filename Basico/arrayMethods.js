09809479c0a9a819954d8cb3b73a55d029d341bb
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-08 10:45:21 +0100

    Terminar los metodos de arrays

--- Basico/arrayMethods.js
+++ Basico/arrayMethods.js
@@ -106,7 +106,7 @@ const studentsFoundByIndex = students.findIndex((student) => student.id === 38);
 // 7. pop --> Elimina el ultimo elemento de un array
 
 // const studentsWithOutLast = students.pop();
-console.log(students);
+// console.log(students);
 
 // 8. splice --> cambia el contenido de un array eliminando elementos existenientes y/o agregando nuevos elementos
 
@@ -125,4 +125,45 @@ const student = {
 	],
 };
 const newStudentList = students.splice(2, 1, student);
-console.log(students);
+// console.log(students);
+
+// 9. includes --> Comprueba si un elemento existe en un array
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers.includes(3));
+
+// 10. reduce --> ejecuta una funcion reductora sobre cada elemento de un array, devolviendo como resultado un unico valor
+
+const sum = numbers.reduce(
+	(accumulator, currentValue) => accumulator + currentValue,
+	0
+);
+
+const ages =
+	students.reduce((accumulator, currentValue) => {
+		return accumulator + currentValue.age;
+	}, 0) / students.length;
+console.log(ages);
+// console.log(sum / numbers.length);
+
+// 11. concat --> Se usa para unir dos o mas arrays. Este metodo no modifica el array original, si no que devuelve uno nuevo.
+
+const newPromoStudent = [
+	{
+		id: 35,
+		name: "Carmen",
+		lastname: "Molina",
+		gender: "Femenino",
+		age: 45,
+		address: [
+			{
+				name: "Calle 5",
+				number: 1245,
+				city: "Madrid",
+			},
+		],
+	},
+];
+
+const allStudents = students.concat(newPromoStudent);
+
+console.log(allStudents);
